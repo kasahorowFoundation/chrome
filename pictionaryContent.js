9a3622bc56f8840d328dd594c08a7e688a1c0ce4
Nuha Khaled <Nuha.khaled.92@gmail.com>
2014-11-16 19:51:18 +0200

    keyboard.js fixed

--- pictionaryContent.js
+++ pictionaryContent.js
@@ -8,30 +8,33 @@ function ReplaceText(x)
    var parent = x.parentNode;
  
    for (var i = 0; i < wordsLength; ++i) {
-     dict_url = 'http://' + language + '.kasahorow.org/app/d?kw='+  words[i] + '&fl='+ language +'&tl=en';
+     dict_url = 'http://lang.kasahorow.org/app/d?kw='+  words[i] + '&fl=lang&tl=en';
      var a = document.createElement('a');
      a.setAttribute('href', dict_url);
-     a.innerHTML = words[i] + " ";
+     a.innerHTML = words[i];
      parent.insertBefore(a, x);
+     parent.insertBefore(document.createTextNode(' '), x);
     }
  
     parent.removeChild(x);
 }
-
-
 
 function Replacer(x){
     var kids = x.childNodes;
   
     for ( var j = kids.length - 1; j >= 0; --j )
       {
+
         if (kids[j].nodeType == 3)
           ReplaceText(kids[j]);
-        else if (kids[j].tagName != "a")
+        else if(typeof kids[j].tagName === 'undefined')
+            Replacer(kids[j]);        
+        else if (kids[j].tagName.toLowerCase() != "a")
           Replacer(kids[j]);
       }
   }
    
+
 
 
 var language = "";
