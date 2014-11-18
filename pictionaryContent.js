ece8b3b7b5e275bc25ebbd86a92d9d331677ea95
kasahorow <git@kasahorow.org>
2014-11-18 08:39:32 +0000

    Fix language setting and open links in a new tab

--- pictionaryContent.js
+++ pictionaryContent.js
@@ -1,4 +1,4 @@
-function ReplaceText(x)
+function ReplaceText(x, lang)
 {
    var str = x.textContent;
 
@@ -8,9 +8,10 @@ function ReplaceText(x)
    var parent = x.parentNode;
  
    for (var i = 0; i < wordsLength; ++i) {
-     dict_url = 'http://lang.kasahorow.org/app/d?kw='+  words[i] + '&fl=lang&tl=en';
+     dict_url = 'http://' + lang + '.kasahorow.org/app/d?kw=' +  words[i] + '&fl=' + lang + '&tl=en&utm_source=chrome&utm_campaign=lit&utm_medium=book';
      var a = document.createElement('a');
      a.setAttribute('href', dict_url);
+     a.setAttribute('target', 'woaka');
      a.innerHTML = words[i];
      parent.insertBefore(a, x);
      parent.insertBefore(document.createTextNode(' '), x);
@@ -19,18 +20,17 @@ function ReplaceText(x)
     parent.removeChild(x);
 }
 
-function Replacer(x){
+function Replacer(x, language){
     var kids = x.childNodes;
   
     for ( var j = kids.length - 1; j >= 0; --j )
       {
-
         if (kids[j].nodeType == 3)
-          ReplaceText(kids[j]);
+          ReplaceText(kids[j], language);
         else if(typeof kids[j].tagName === 'undefined')
-            Replacer(kids[j]);        
+            Replacer(kids[j], language);
         else if (kids[j].tagName.toLowerCase() != "a")
-          Replacer(kids[j]);
+          Replacer(kids[j], language);
       }
   }
    
@@ -40,7 +40,7 @@ function Replacer(x){
 var language = "";
 chrome.runtime.sendMessage({method: "getLang"}, function(response) {
 	language= response.lang;
-  Replacer(document.body);
+  Replacer(document.body, language);
 
 });
 
