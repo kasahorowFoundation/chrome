cb5e2e2ea8e0d3e0db3a6d53b794927f8427f94d
Nuha Khaled <Nuha.khaled.92@gmail.com>
2015-02-07 04:01:52 +0200

    new Tab time and notification

---
+++ NewTab.js
@@ -0,0 +1,48 @@
+function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('currentTime').innerHTML = h+":"+m+":"+s;
+    var t = setTimeout(function(){startTime()},500);
+}
+
+function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+
+window.addEventListener('load', startTime);
+
+
+
+function getNotification(){
+  url_link = 'http://' + localStorage.lang + '.kasahorow.org/app/m?format=json&source=chrome';
+
+          $.ajax({
+              url:url_link, 
+              dataType:"JSON",
+              async:false,
+              success:function(r){ 
+          
+          $('#by').text(r["by"]);
+          $('#inspiration').text(r["inspiration"]);
+          $('#day').text( r["day"]);
+
+
+              }
+
+
+          });
+
+$('#notification').show().fadeIn(1000);
+          //return r;
+ }
+
+
+ window.addEventListener('load', getNotification);
+
+
