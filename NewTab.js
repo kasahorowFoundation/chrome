ecc424cdbea503ead42c606e715e6fe2a3b85234
Nuha Khaled <Nuha.khaled.92@gmail.com>
2015-02-15 19:22:49 +0200

    random background code added commented

--- NewTab.js
+++ NewTab.js
@@ -35,7 +35,20 @@ function getNotification()
     $('#notification').show().fadeIn(1000);
 }
 
+/*
+function backgroundRandom(){
+    var bgArray = ['http://goo.gl/Ir6y9D','http://goo.gl/jZsq5N','http://goo.gl/ZtTu3k','http://goo.gl/UEZF6m','http://goo.gl/Id0ak9','http://goo.gl/PaFNLs','http://goo.gl/hTvKpR','http://goo.gl/53bnNL','http://goo.gl/GkMbgt','http://goo.gl/U0OCiy','http://goo.gl/8XLCoq'];
+    var bg = "url('"+bgArray[Math.floor(Math.random() * bgArray.length)]+"')";
+    // If you have defined a path for the images
+                var path = 'images/bg/';
 
+                // then you can put it right before the variable 'bg'
+                $('body').css('background', path+bg);
+
+                $('body').css('background-image', bg);
+}
+*/
 //called on page load
 window.addEventListener('load', startTime);
 window.addEventListener('load', getNotification);
+//window.addEventListener('load', backgroundRandom);
